e698661ee6b85d32993b4b871ac29751afe66f7c
biojwaas <jonas.waas@uni-bielefeld.de>
2022-05-28 10:22:19 +0200

    Fixed error Message, if no argument was given.

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import pathlib
+import sys
 
 from src.app import app
 from src.components import ComponentHandler
@@ -8,9 +9,14 @@ from src.input_files import ARGS, FilesHandler
 
 
 def __start_application(args):
-    handler = FilesHandler.FileHandler(args)
-    component_handler = ComponentHandler.Component(handler)
-    app.AppHandler(pathlib.Path.absolute(args.get_absolut_path('dir')), component_handler, args.get_port())
+    if args.has_option('dir'):
+        handler = FilesHandler.FileHandler(args)
+        component_handler = ComponentHandler.Component(handler)
+        app.AppHandler(pathlib.Path.absolute(args.get_absolut_path('dir')), component_handler, args.get_port())
+    else:
+        sys.stderr.write('error: No Argument was set.')
+        args.parser.print_help()
+        sys.exit(1)
 
 
 if __name__ == '__main__':
